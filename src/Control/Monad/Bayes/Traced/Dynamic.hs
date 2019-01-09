23e54688dea931f2c8b38c4ceafdfad68a068870
Adam Massmann <akm2203@columbia.edu>
2019-01-09 15:16:08 -0500

    Updated stack to lts-13.0, all tests passed

--- src/Control/Monad/Bayes/Traced/Dynamic.hs
+++ src/Control/Monad/Bayes/Traced/Dynamic.hs
@@ -93,9 +93,9 @@ mh n (Traced c) = do
   (m,t) <- c
   let f 0 = return [t]
       f k = do
-        x:xs <- f (k-1)
-        y <- mhTrans m x
-        return (y:x:xs)
+        xs <- f (k-1)
+        y <- mhTrans m (head xs)
+        return (y:xs)
   ts <- f n
   let xs = map output ts
   return xs
